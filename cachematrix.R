8caac527fd2764265d7d3c9eddd695409d7c20b4
Mehmet Eren Erdoğan <114462338+MerenErdogan@users.noreply.github.com>
2022-10-12 07:36:03 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { ##This function will allow the code to retrieve matrixes.
+  invMatrix <- NULL
+                                         ##Setting invMatrix to NULL will lead the code to get data from getmatrix below. 
+  setMatrix <- function(y) {
+    x <<- y
+    invMatrix <<- NULL
+  }
+  
+  getMatrix <- function() x                             
+  setInverse <- function(inverse) invMatrix <<- inverse  ##This part will retrieve the "x" matrix and take inverse of it.
+  getInverse <- function() invMatrix                     
+  list(setMatrix = setMatrix, getMatrix = getMatrix,      ##This part is necessary to use $sign and link both steps of the process.
+       setInverse = setInverse, getInverse = getInverse)
+  
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {             ##This part will paste the same inversed matrix if x is NULL.
+  
+  invMatrix <- x$getInverse()                ##If not, it will take inverse of the given matrix.
+  if(!is.null(invMatrix)) {                       
+    message("Getting Cached Invertible Matrix")    
+    return(invMatrix)                             
+  }
+  
+  MatrixData <- x$getMatrix()                     
+  invMatrix <- solve(MatrixData, ...)            
+  x$setInverse(invMatrix)                        
+  return(invMatrix)                         
 }
+
+TestMatrix <- matrix(1:4,2,2)
+
+CacheMatrix <- makeCacheMatrix(TestMatrix)
+CacheMatrix$getMatrix()
+CacheMatrix$getInverse()
+
+cacheSolve(CacheMatrix)
+cacheSolve(CacheMatrix)
